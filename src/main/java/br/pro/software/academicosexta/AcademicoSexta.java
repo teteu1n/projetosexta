7c0021a69532ab39967df262e79c64dbaf623447
Anderson Sanches <andycds@gmail.com>
2024-04-26 22:52:52 -0300

    Tela login

---
+++ src/main/java/br/pro/software/academicosexta/AcademicoSexta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.pro.software.academicosexta;
+
+/**
+ *
+ * @author andyc
+ */
+public class AcademicoSexta {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
