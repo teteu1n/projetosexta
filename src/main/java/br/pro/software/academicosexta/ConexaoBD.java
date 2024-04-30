4e6607cc79eea8fc08fab1cdd02a56db5866c563
Anderson Sanches <andycds@gmail.com>
2024-04-30 11:37:41 -0300

    Conexão BD

---
+++ src/main/java/br/pro/software/academicosexta/ConexaoBD.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.pro.software.academicosexta;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author andyc
+ */
+public class ConexaoBD {
+    private static String host = "localhost";
+    private static String porta = "3306";
+    private static String db = "academico";
+    private static String usuario = "root";
+    private static String senha = "root"; // você coloca anima123
+
+    public static Connection obterConexao() throws Exception{
+        String url = String.format(
+          "jdbc:mysql://%s:%s/%s",
+            host,
+            porta,
+            db
+       );
+    return DriverManager.getConnection(url, usuario, senha);
+    }
+}
