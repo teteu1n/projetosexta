e2867a0d6c8238c980317ae7521a69b2f26fd117
Matheus Nascimento <80324098+teteu1n@users.noreply.github.com>
2024-06-05 23:08:38 -0300

    Update Banco.sql

--- src/main/java/br/pro/software/academicosexta/Banco.sql
+++ src/main/java/br/pro/software/academicosexta/Banco.sql
@@ -1,6 +1,6 @@
-create database academico;
 
-use academico;
+
+use CPT;
 
 create table tb_usuario (
 	id int primary key auto_increment, 
@@ -9,51 +9,16 @@ create table tb_usuario (
 );
 
 insert into tb_usuario (nome, senha) values ('admin', 'admin');
-insert into tb_usuario (nome, senha) values ('lili', 'cachorra');
+
 select * from tb_usuario;
 
-create table tb_curso(
+create table tb_materias(
 	id int primary key auto_increment,
 	nome varchar(200) not null,
-	tipo varchar(200) not null
+	link_videoaula varchar(255) not null,
+    link_exercicio varchar(255) not null
 );
 
-select * from tb_curso;
+select * from tb_materias;
 
-insert into tb_curso(nome, tipo) values ('Ciência da Computação', 'Bacharelado');
-insert into tb_curso(nome, tipo) values ('Análise e Desenvolvimento de Sistemas', 'Tecnólogo');
-insert into tb_curso(nome, tipo) values ('Engenharia de Software', 'Bacharelado');
-
-create table tb_aluno(
-	RA int primary key,
-	nome varchar(200) not null,
-	ano_nascimento int not null
-);
-
-INSERT INTO tb_aluno (RA, nome, ano_nascimento) VALUES
-       (123456, "João Henrique", 2000),
-       (345678, "Ana Maria", 2001),
-       (875321, "Paulo Rogerio", 2000),
-       (456789, "Maria Regina", 1999)
-;
-select * from tb_aluno;
-
-create table tb_aluno_curso (
-	id_curso int not null,
-    RA_aluno int not null,
-    ano int not null,
-    semestre int not null,
-    primary key (id_curso, RA_aluno),
-    constraint fk_aluno
-		foreign key (RA_aluno)
-        references tb_aluno (RA)
-        on delete no action
-        on update cascade,
-	constraint fk_curso
-		foreign key (id_curso)
-        references tb_curso (id)
-        on delete no action
-        on update cascade
-);
-INSERT INTO tb_aluno_curso VALUES (1, 123456, 2020, 1);
-select * from tb_aluno_curso;
+insert into tb_materias(nome, link_videoaula, link_exercicio) values ('Matemática', 'https://www.youtube.com/watch?v=ddZHkCUcYRM&pp=ygUcdmlkZW8gYXVsYSBtYXRlbWF0aWNhIGJhc2ljYQ%3D%3D', 'https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwist-uL3cWGAxXHqpUCHbq9IAoQFnoECAYQAQ&url=https%3A%2F%2Fwww.todamateria.com.br%2Fexercicios-de-matematica-basica%2F&usg=AOvVaw2OYPMy4AnszrkZFb4eSYT3&opi=89978449');
